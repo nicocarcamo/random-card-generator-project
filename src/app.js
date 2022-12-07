31b705a151772262b8a00d2640da433e4b53a0e6
nicocarcamo <nicolascarcamonaccarino@gmail.com>
2022-12-07 19:46:03 +0000

    project upload

--- src/app.js
+++ src/app.js
@@ -2,10 +2,65 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let cardNum = num => {
+  if (num >= 2 && num <= 10) {
+    return num;
+  } else {
+    switch (num) {
+      case 1:
+        return "A";
+        break;
+      case 11:
+        return "J";
+        break;
+      case 12:
+        return "Q";
+        break;
+      case 13:
+        return "K";
+        break;
+    }
+  }
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let randomType = () => {
+  let type = Math.floor(Math.random() * 4) + 1;
+  switch (type) {
+    case 1:
+      return "♦";
+      break;
+    case 2:
+      return "♥";
+      break;
+    case 3:
+      return "♠";
+      break;
+    case 4:
+      return "♣";
+      break;
+  }
+};
+
+let randomNumberGenerator = () => {
+  let num = Math.floor(Math.random() * 13) + 1;
+  return num;
+};
+
+window.newCard = function newCard() {
+  document.getElementById("card-number").innerHTML = cardNum(
+    randomNumberGenerator()
+  );
+  let randomtype = randomType();
+  let suitUp = document.getElementById("typeUp");
+  let suitDown = document.getElementById("typeDown");
+
+  if (randomtype === "♦" || randomtype === "♥") {
+    suitUp.innerHTML = randomtype;
+    suitDown.innerHTML = randomtype;
+    suitUp.style.color = "red";
+    suitDown.style.color = "red";
+  } else {
+    suitUp.innerHTML = randomtype;
+    suitDown.innerHTML = randomtype;
+  }
 };
